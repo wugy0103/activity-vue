8e96de046c3e0f8e20f8a30c7e0552702d0c1713
wuguoyuan <946296536@qq.com>
2017-10-25 21:16:22 +0800

    add 打包

--- src/config/ipConfig.js
+++ src/config/ipConfig.js
@@ -2,11 +2,14 @@
  * 系统相关配置
  */
 const ipConfig = {
-  // sso登录地址
-  // ssoBaseUrl: 'http://beta3-ucenter.daxmall.com/',
-  // mShopUrl: 'http://beta3-m.daxmall.com/',
   ssoBaseUrl: 'http://dev-ucenter.daxmall.com:9999/',
   mShopUrl: 'http://192.168.60.11:8080/',
   apiBaseUrl: 'http://192.168.60.11:8184/'
+  // // sso登录地址
+  // ssoBaseUrl: 'http://beta2-ucenter.daxmall.com/',
+  // // 商城接口地址
+  // mShopUrl: 'http://beta2-m.daxmall.com/',
+  // // 接口基础地质
+  // apiBaseUrl: 'http://beta2-pms-mobile.daxmall.com/'
 }
 export default ipConfig

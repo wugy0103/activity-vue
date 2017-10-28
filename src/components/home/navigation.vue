4ccf56cd95255a912adda05cfe09ea5a58a1564d
wuguoyuan <946296536@qq.com>
2017-10-28 17:16:53 +0800

    add 锚点颜色

--- src/components/home/navigation.vue
+++ src/components/home/navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navigation" :class="{ active: showFlag === true, fixed: fixed === true}">
+  <div class="navigation" :class="{ active: showFlag === true, fixed: fixed === true, whileStyle: false}">
     <div style="overflow:hidden;">
       <ul class="clear anchor">
         <li v-for="(item, index) of anchorContentList" :class="{ active: item.templateId === anchorLocation }" :key="item.templateId" @click="activeLi(item.templateId,index)">{{ item.templateName }}</li>
@@ -93,99 +93,133 @@ export default {
 <style lang="scss" scoped>
 @import '../../style/common';
 .navigation {
+  width: 100%;
+  height: 0.4rem;
+  background: #464854;
+  position: relative;
+  &.active::before {
+    content: '切换楼层';
+    position: absolute;
+    z-index: 1;
     width: 100%;
     height: 0.4rem;
+    left: 0rem;
+    top: 0rem;
+    font-size: 0.14rem;
+    color: #737787;
+    line-height: 0.4rem;
+    padding-left: 0.1rem;
+    box-sizing: border-box;
     background: #464854;
-    position: relative;
-    &.active::before {
-      content: '切换楼层';
-      position: absolute;
-      z-index: 1;
-      width: 100%;
+  }
+  &.fixed {
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 10;
+  }
+  ul.anchor {
+    height: 0.4rem;
+    overflow: hidden;
+    width: 200%;
+  }
+  ul.anchor>li {
+      float: left;
       height: 0.4rem;
-      left: 0rem;
-      top: 0rem;
-      font-size: 0.14rem;
-      color: #737787;
       line-height: 0.4rem;
-      padding-left: 0.1rem;
-      box-sizing: border-box;
-      background: #464854;
-    }
-    &.fixed {
-      position: fixed;
+      margin-left: 0.25rem;
+      font-size: 0.14rem;
+      color: #E4E7F2;
+      position: relative;
+      cursor: pointer;
+      &.active::before {
+          content: '';
+          position: absolute;
+          width: 0.25rem;
+          height: 0.4rem;
+          left: -0.25rem;
+          top: -0.01rem;
+          background: url(../../images/icon_dingwie_red@2x.png) no-repeat center;
+          background-size: 0.1rem 0.13rem;
+      }
+      &.active {
+        color: #fff;
+      }
+  }
+  &>a {
+      position: absolute;
+      z-index: 2;
+      right: 0;
       top: 0;
-      left: 0;
-      z-index: 10;
-    }
-    ul.anchor {
+      background: #464854;
+      box-shadow: inset 0 0 0 0 #E4E7F2;
+      border-left: 0.5px solid #999;
+      width: 0.4rem;
       height: 0.4rem;
-      overflow: hidden;
-      width: 200%;
+      background:#464854 url(../../images/icon_down@2x.png) no-repeat center;
+      background-size: 0.12rem 0.07rem;
+      &.active {
+        transform: rotate(180deg)
+      }
+  }
+  ul.anchorContent {
+      width: 100%;
+      position: absolute;
+      z-index: 10;
+      top: 0.4rem;
+      left: 0;
+      background: rgba(70,72,84,0.95);
+      &>li {
+          float: left;
+          width: 33.333333%;
+          height: 0.4rem;
+          line-height: 0.4rem;
+          text-align: center;
+          font-size: 0.14rem;
+          color: #E4E7F2;
+          cursor: pointer;
+          &.active::before {
+              content: '';
+              position: absolute;
+              width: 0.25rem;
+              height: 0.4rem;
+              margin-left: -0.25rem;
+              margin-top: -0.01rem;
+              background: url(../../images/icon_dingwie_red@2x.png) no-repeat center;
+              background-size: 0.1rem 0.13rem;
+          }
+          &.active {
+            color: #fff;
+          }
+      }
+  }
+  &.whileStyle {
+    background: #FFFFFF;
+    box-shadow: 0 2px 6px 0 #E4E7F2;
+    &.active::before {
+      color: #737787;
+      background: #FFFFFF;
     }
     ul.anchor>li {
-        float: left;
-        height: 0.4rem;
-        line-height: 0.4rem;
-        margin-left: 0.25rem;
-        font-size: 0.14rem;
-        color: #FFFFFF;
-        position: relative;
-        cursor: pointer;
-        &.active::before {
-            content: '';
-            position: absolute;
-            width: 0.25rem;
-            height: 0.4rem;
-            left: -0.25rem;
-            top: -0.01rem;
-            background: url(../../images/icon_dingwie_red@2x.png) no-repeat center;
-            background-size: 0.1rem 0.13rem;
-        }
+      color: #464854;
+      &.active {
+        color: #FF2C29;
+      }
     }
     &>a {
-        position: absolute;
-        z-index: 2;
-        right: 0;
-        top: 0;
-        background: #464854;
-        box-shadow: inset 0 0 0 0 #E4E7F2;
-        border-left: 0.5px solid #999;
-        width: 0.4rem;
-        height: 0.4rem;
-        background:#464854 url(../../images/icon_down@2x.png) no-repeat center;
-        background-size: 0.12rem 0.07rem;
-        &.active {
-          transform: rotate(180deg)
-        }
+      background-color: #ffffff;
+      box-shadow: inset 0 0 0 0 #E4E7F2;
+      border-left: 0.5px solid #E4E7F2;
     }
     ul.anchorContent {
-        width: 100%;
-        position: absolute;
-        z-index: 10;
-        top: 0.4rem;
-        left: 0;
-        background: rgba(70,72,84,0.95);
-        &>li {
-            float: left;
-            width: 33.333333%;
-            height: 0.4rem;
-            line-height: 0.4rem;
-            text-align: center;
-            font-size: 0.14rem;
-            color: #FFFFFF;
-            cursor: pointer;
-            &.active::before {
-                content: '';
-                position: absolute;
-                width: 0.25rem;
-                height: 0.4rem;
-                margin-left: -0.25rem;
-                margin-top: -0.01rem;
-                background: url(../../images/icon_dingwie_red@2x.png) no-repeat center;
-                background-size: 0.1rem 0.13rem;
-            }
+      background: rgba(255,255,255,0.95);
+      &>li {
+        color: #464854;
+        &.active {
+          color: #FF2C29;
         }
+      }
     }
+  }
 }
 </style>

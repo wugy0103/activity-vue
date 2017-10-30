b846e66e140a69893984beaf703a7a7c32e38513
wuguoyuan <946296536@qq.com>
2017-10-30 19:42:11 +0800

    edit 静态页面优化

--- src/views/mall/20171026.vue
+++ src/views/mall/20171026.vue
@@ -2,7 +2,9 @@
   <div class="layout">
     <share v-if="topic.shareTitle" :title="topic.shareTitle" :desc="topic.shareDescription" :imgUrl="topic.wxPreviewPic" :link="link" :longPageUrl="longPageUrl" :sharePic="topic.activitySharePic || '/static/images/share.png'"></share>
     <singleTemplate :imgArr="[{ templatePic: '/static/images/coup1.jpg' }]"></singleTemplate>
-    <singleTemplate :imgArr="[{ templatePic: '/static/images/coup2.jpg' }]"></singleTemplate>
+    <div @click="getActiveCoupon(status, ipConfig.apiBaseUrl + 'couponApi/getActiveCoupon/121/41')">
+      <singleTemplate :imgArr="[{ templatePic: '/static/images/coup2.jpg' }]"></singleTemplate>
+    </div>
     <singleTemplate :imgArr="[{ templatePic: '/static/images/coup3.jpg' }]"></singleTemplate>
     <singleTemplate :imgArr="[{ templatePic: '/static/images/coup4.jpg' }]"></singleTemplate>
     <iconTemplate></iconTemplate>
@@ -13,12 +15,19 @@
 import singleTemplate from '../../components/home/singleTemplate.vue'
 import iconTemplate from '../../components/home/iconTemplate.vue'
 import share from '../../components/common/share.vue'
-
+// 使用mint-ui
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+import axios from 'axios'
+import ipConfig from '../../config/ipConfig'
 export default {
   components: {
     singleTemplate,
     iconTemplate,
-    share
+    share,
+    MintUI,
+    axios,
+    ipConfig
   },
   data () {
     return {
@@ -27,13 +36,42 @@ export default {
       topic: {
         shareTitle: '恭喜你通关，请领取99元商城优惠券',
         shareDescription: '游戏通关领券，商城购物满199立减99元。',
-        wxPreviewPic: 'http://activity.daxmall.com/mall/img/20171026/coup1.jpg' || 'https://api.healthmall.cn/logo.jpg'
-      }
+        wxPreviewPic: window.location.origin + '/static/images/coup1.jpg' || 'https://api.healthmall.cn/logo.jpg'
+      },
+      status: 1
+    }
+  },
+  computed: {
+    ipConfig () {
+      return ipConfig
     }
   },
   methods: {
+    getActiveCoupon (status, url) {
+      console.log(status, url)
+      MintUI.Indicator.open()
+      axios.get(url).then((res) => {
+        console.log(res)
+        MintUI.Indicator.close()
+        MintUI.Toast({
+          message: res.data.message || '接口异常！',
+          className: 'Toast'
+        })
+      }, (rej) => {
+        console.log(url, rej)
+        MintUI.Indicator.close()
+        console.log(rej.response.status)
+        if (rej.response.status !== 401) {
+          MintUI.Toast({
+            message: '网络繁忙！',
+            className: 'Toast'
+          })
+        }
+      })
+    }
   },
   created () {
+    document.title = '恭喜你通关，请领券'
   },
   mounted () {
   }
